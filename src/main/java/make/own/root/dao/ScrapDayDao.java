7e2a7d4f40b3d8f2e4745faacf2e6997568c4b89
TEMA <tema_@naver.com>
2020-06-11 23:35:00 +0900

    [mod] ScrapDay, ScrapPlace Dao에 Mapper 변경에 맞는 메서드 추가

--- src/main/java/make/own/root/dao/ScrapDayDao.java
+++ src/main/java/make/own/root/dao/ScrapDayDao.java
@@ -8,11 +8,14 @@ import make.own.root.vo.ScrapDay;
 public interface ScrapDayDao {
 
   // ScrapDayServiceImpl addReviewDay(int userNo, int reviewDayNo)에서 호출
-  int insert(Map<String, Object> scrap) throws Exception;
+  Integer insert(Map<String, Object> scrap) throws Exception;
 
   // ScrapDayServiceImpl list(int userNo)에서 호출
   List<ScrapDay> findAll(int userNo) throws Exception;
 
   // ScrapDayServiceImpl delete(int reviewDayNo)에서 호출
   int delete(int reviewDayNo) throws Exception;
+
+  //ScrapDayServiceImpl find(int userNo, int reviewDayNo)에서 호출
+  List<ScrapDay> findScrapDay(Map<String, Object> scrap) throws Exception;
 }
--- src/main/java/make/own/root/dao/ScrapPlaceDao.java
+++ src/main/java/make/own/root/dao/ScrapPlaceDao.java
@@ -7,11 +7,14 @@ import make.own.root.vo.ScrapPlace;
 public interface ScrapPlaceDao {
 
   // ScrapPlaceServiceImpl addReviewPlace(int userNo, int reviewPlaceNo)에서 호출
-  int insert(Map<String, Object> scrap) throws Exception;
+  Integer insert(Map<String, Object> scrap) throws Exception;
 
   // ScrapPlaceServiceImpl list(int scrapDayNo)에서 호출
   List<ScrapPlace> findAll(int userNo) throws Exception;
 
   // ScrapPlaceServiceImpl delete(int reviewPlaceNo)에서 호출
   int delete(int reviewPlaceNo) throws Exception;
+
+  //ScrapPlaceServiceImpl find(int userNo, int reviewPlaceNo)에서 호출
+  List<ScrapPlace> findScrapPlace(Map<String, Object> scrap) throws Exception;
 }
